ba1f3ca1a8e6e237b4b9d3fe7602b20fb32a787c
Bryce Hostetler <bhostetler@ufl.edu>
2021-03-10 12:58:49 -0500

    test bootstrap, basic landing page

--- UFSailWebsite/UFSailWebsite/settings.py
+++ UFSailWebsite/UFSailWebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # our apps
-    'TestApp',
+    'Information',
 
     # default/utility
     'django.contrib.admin',
@@ -138,3 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/bootstrap"),
+)
